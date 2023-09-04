45e106fa321c09ce9ea962c183fdba7c07ae888b
andrey <anvolkov_1@edu.hse.ru>
2023-09-04 18:06:15 +0300

    initial

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, os
+
+app = Flask(__name__)
+
+@app.route('/hostname', methods=['GET'])
+def get_hostname():
+    return jsonify({'hostname': os.uname().nodename})
+
+@app.route('/author', methods=['GET'])
+def get_author():
+    author = os.getenv('AUTHOR', 'Unknown')
+    return jsonify({'author': author})
+
+@app.route('/id', methods=['GET'])
+def get_uuid():
+    uuid = os.getenv('UUID', 'Unknown')
+    return jsonify({'uuid': uuid})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
